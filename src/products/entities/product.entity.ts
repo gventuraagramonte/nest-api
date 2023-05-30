4a2c72d178a35a0439d00a94248cd6ec7b99721c
Giorgio Ventura <gventuraagramonte@gmail.com>
2023-05-30 05:19:39 -0500

    Se configura las entidades para almacenar info en bd mysql

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,35 @@
-export class Product {}
+// Crea la referencia en la base de datos (TABLA)
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column('varchar', {
+        unique: true,
+    })
+    title: string
+
+    @Column('numeric', { default: 0 })
+    price: number
+
+    @Column({ type: 'varchar', nullable: true })
+    description: string
+
+    @Column('varchar', {
+        unique: true
+    })
+    slug: string
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number
+
+    @Column('simple-array')
+    sizes: string[]
+
+    @Column('varchar')
+    gender: string
+}
