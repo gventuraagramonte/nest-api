406cd0a0ba8a3f1f9c2a42bc58096ba9564c8402
Giorgio Ventura <gventuraagramonte@gmail.com>
2023-05-31 09:58:02 -0500

    Paginacion y actualizacion patch

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid'
 
 @Injectable()
 export class ProductsService {
@@ -28,19 +30,48 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return this.productRepository.find({});
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto
+    return this.productRepository.find({
+      take: limit,
+      skip: offset
+      // TODO: relaciones
+    });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id })
+  async findOne(term: string) {
+
+    let product: Product
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term })
+    } else {
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('UPPER(title) =:title or slug =:slug', {
+          title: term.toUpperCase(),
+          slug: term.toLowerCase()
+        }).getOne()
+    }
     if (!product)
-      throw new NotFoundException(`Product with id ${id} not found`)
+      throw new NotFoundException(`Product with id ${term} not found`)
     return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id: id,
+      ...updateProductDto
+    })
+    if (!product) throw new NotFoundException(`Product with id ${id} not found`)
+
+    try {
+
+      await this.productRepository.save(product)
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error)
+    }
   }
 
   async remove(id: string) {
